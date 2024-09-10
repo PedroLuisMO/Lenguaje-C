2aa35ec2a4c82349fc57742e684d6180a82f277f
Luis <pedroluis.moor@gmail.com>
2024-09-10 16:59:47 -0600

    se dejo el minimo main

--- DataStructures/LinkedList/Circular/main.c
+++ DataStructures/LinkedList/Circular/main.c
@@ -9,12 +9,7 @@ int main (void)
     linked = createClinked ();
     if (linked == NULL)
         return -1;
-    insertAtBegining(linked,2);
-    insertAtBegining(linked,1);
-    insertAtEnd(linked,6);
-    deleteAtMiddle(linked,2);
-    display(linked);
-
+     
     freeClinked(linked);
 
     return 0;
